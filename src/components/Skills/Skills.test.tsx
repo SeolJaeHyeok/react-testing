ae71927b1c5eeed2d5b6c5487cc575bd088a7672
jaehyeok <milkboy2564@naver.com>
2023-04-17 16:10:10 +0900

    findby, findAllBy

--- src/components/Skills/Skills.test.tsx
+++ src/components/Skills/Skills.test.tsx
@@ -1,36 +1,49 @@
-import {render, screen} from "@testing-library/react"
-import Skills from "./Skills";
+import { render, screen } from '@testing-library/react';
+import Skills from './Skills';
 
 describe('Skills', () => {
   const skills = ['html', 'css', 'javascript'];
 
   test('올바르게 Skills 컴포넌트를 렌더링한다.', () => {
-    render(<Skills skills={skills}/>)
+    render(<Skills skills={skills} />);
 
     const listEl = screen.getByRole('list');
     expect(listEl).toBeInTheDocument();
   });
 
   test('Skills 아이템 목록을 렌더링한다.', () => {
-    render(<Skills skills={skills}/>);
+    render(<Skills skills={skills} />);
 
     const listEls = screen.getAllByRole('listitem');
-    expect(listEls).toHaveLength(skills.length)
+    expect(listEls).toHaveLength(skills.length);
   });
 
   test('Login Button을 렌더링한다.', () => {
-    render(<Skills skills={skills}/>)
+    render(<Skills skills={skills} />);
 
     const buttonEl = screen.getByRole('button', {
-      name: "Login",
+      name: 'Login',
     });
     expect(buttonEl).toBeInTheDocument();
-  })
+  });
 
   test('Start learning Button을 렌더링 하지 않는다.', () => {
     render(<Skills skills={skills} />);
 
     const startLearningButton = screen.queryByRole(/start learning/i);
     expect(startLearningButton).not.toBeInTheDocument();
-  })
-})
+  });
+
+  test('Start learning Button이 최후에 렌더링 된다.', async () => {
+    render(<Skills skills={skills} />);
+
+    const startLearningButton = await screen.findByRole(
+      'button',
+      {
+        name: 'Start learning',
+      },
+      { timeout: 2000 }
+    );
+    expect(startLearningButton).toBeInTheDocument();
+  });
+});
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,14 +1,24 @@
-import { useState } from "react";
-import { SkillsProps } from "./Skills.types";
+import { useEffect, useState } from 'react';
+import { SkillsProps } from './Skills.types';
 
 export default function Skills(props: SkillsProps) {
-  const {skills} = props;
+  const { skills } = props;
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return <>
-    <ul>
-      {skills.map((skill) => <li key={skill}>{skill}</li>)}
-    </ul>
-    {isLoggedIn ? <button>Start learning</button> : <button onClick={() => setIsLoggedIn(true)}>Login</button>}
-  </>
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoggedIn(true);
+    }, 1500);
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {skills.map((skill) => (
+          <li key={skill}>{skill}</li>
+        ))}
+      </ul>
+      {isLoggedIn ? <button>Start learning</button> : <button onClick={() => setIsLoggedIn(true)}>Login</button>}
+    </>
+  );
 }
