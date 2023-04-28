deb2b11d08ff52ac0710a1efb09a80b13ce1b01f
Jaehyeok <milkboy2564@naver.com>
2023-04-28 21:00:52 +0900

    fireEvent를 통한 사용자 상호작용 테스트

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -1,9 +1,19 @@
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+  const buttonRef = useRef(null);
+
+  const handleButtonClick = () => {
+    buttonRef.current.backgroundColor = 'blue';
+    buttonRef.current.textContent = 'Change to Red';
+  };
+
   return (
     <div>
-      <button style={{ backgroundColor: 'red' }}>Change to Blue</button>
+      <button ref={buttonRef} onClick={handleButtonClick} style={{ backgroundColor: 'red' }}>
+        Change to Blue
+      </button>
     </div>
   );
 }
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('Change to Blue가 적힌 버튼을 렌더링한다.', () => {
@@ -29,9 +29,24 @@ test('Change to Blue가 적힌 버튼을 렌더링한다.', () => {
   });
 
   // 실패
-  expect(colorButton).toHaveStyle({
-    'background-color': '이건 실패해야지',
-  });
+  // expect(colorButton).toHaveStyle({
+  //   'background-color': '이건 실패해야지',
+  // });
 });
 
-test('버튼을 클릭하면 파란색으로 변경된다.', () => {});
+// Unit Test의 경우 테스트 당 하나의 단언문으로 작성하는 것이 더 적절할 수 있다.
+// 각 테스트의 단언문이 하나일 때 장점은 하나의 테스트가 실패하면 아래의 다른 테스트들이 수행되지 않는다는 점이다.
+// 반면 기능 테스트는 버튼을 클릭하고 체크박스를 선택하고 폼에 입력하는 일련의 과정을 테스트 하는 것이기 때문에
+// 여러 테스트들이 describe 혹은 it 함수 안에서 작성이 되는 것이 적절하다.
+test('버튼을 클릭하면 파란색으로 변경된다.', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', {
+    name: 'Change to Blue',
+  });
+
+  // Click Event 발생
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveTextContent('Change to Red');
+});
