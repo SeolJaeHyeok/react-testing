06296a70a361b0940e350368973bc6ba012ec25c
jaehyeok <milkboy2564@naver.com>
2023-05-18 10:41:48 +0900

    React Bootstrap 적용

--- sundaes-on-demand/src/pages/summary/SummaryForm.jsx
+++ sundaes-on-demand/src/pages/summary/SummaryForm.jsx
@@ -1,19 +1,29 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export default function SummaryForm() {
-  const [checked, setChecked] = useState(false);
+  const [tcChecked, setTcChecked] = useState(false);
 
-  const handleChangTermsAndConditions = (e) => {
-    setChecked(e.target.checked);
-  };
+  const checkboxLabel = (
+    <span>
+      본 <span style={{ color: 'blue' }}>이용약관</span>에 동의합니다.
+    </span>
+  );
 
   return (
-    <form>
-      <label htmlFor='tnc'>이용 약관</label>
-      <input id='tnc' onChange={handleChangTermsAndConditions} type='checkbox' />
-      <button type='submit' disabled={!checked}>
+    <Form>
+      <Form.Group controlId='이용약관'>
+        <Form.Check
+          type='checkbox'
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant='primary' type='submit' disabled={!tcChecked}>
         주문 확인
-      </button>
-    </form>
+      </Button>
+    </Form>
   );
 }
